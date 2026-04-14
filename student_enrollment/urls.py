0cd45f1e18d847f218a82f4413a5579d2333ddcc
Chaitra-Navalgund <chaitranavalgund30@gmail.com>
2026-04-14 16:03:25 +0530

    Fix uploaded image URLs on Render

--- student_enrollment/urls.py
+++ student_enrollment/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,4 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('django_admin/', admin.site.urls),
     path('', include('enrollment.urls')),
+    re_path(r'^images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
